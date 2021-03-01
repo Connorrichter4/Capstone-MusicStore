a04e011e57e281d56fcaee33200051c4cb81fc79
Connorrichter4 <Connorrichter4@gmail.com>
2021-03-01 11:17:41 -0600

    added userController, userRepository, and UserService, updated User to have Long Id

--- src/main/java/com/hcl/capstone/controllers/UserController.java
+++ src/main/java/com/hcl/capstone/controllers/UserController.java
@@ -1,8 +1,34 @@
 package com.hcl.capstone.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.hcl.capstone.entities.User;
 
 @Controller
 public class UserController {
-
+	
+	private Logger logger = LoggerFactory.getLogger(UserController.class);
+	
+	@GetMapping("/login")
+	public String showLogin() {
+		return "login";
+	}
+	
+	@GetMapping("/signup")
+	public String showSignUp() {
+		return "signup";
+	}
+	
+	@PostMapping("/signup")
+	public ModelAndView postSignUp(ModelMap model, User user) {
+		logger.info(user.toString());
+		
+		return new ModelAndView("redirect:/", model);
+	}
 }
--- src/main/java/com/hcl/capstone/services/UserService.java
+++ src/main/java/com/hcl/capstone/services/UserService.java
@@ -1,8 +1,33 @@
 package com.hcl.capstone.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.crypto.password.PasswordEncoder; (keep for security)
 import org.springframework.stereotype.Service;
+
+import com.hcl.capstone.entities.User;
+import com.hcl.capstone.repositories.UserRepository;
 
 @Service
 public class UserService {
 
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	
+//	@Autowired (saving for security)
+//	PasswordEncoder passwordEncoder;
+	
+	@Autowired
+	UserRepository userRepository;
+	
+	public User createUser(User user) {
+//		user.setPassword(passwordEncoder.encode(user.getPassword())); (keep for security)
+		user.toString();
+		return userRepository.save(user);
+	}
+	
+	public User getUserByEmail(String email) {
+		return userRepository.findUserByEmail(email);
+	}
+	
 }
