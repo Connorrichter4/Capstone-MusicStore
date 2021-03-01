be955d6ebb754c0fb9af26a6e60a17634a7ca9f4
Connorrichter4 <Connorrichter4@gmail.com>
2021-02-28 18:44:01 -0600

    added data.sql file

--- src/main/java/com/hcl/capstone/entities/User.java
+++ src/main/java/com/hcl/capstone/entities/User.java
@@ -25,6 +25,77 @@ public class User {
 	private String credit_card;
 	
 	private String role;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getZipcode() {
+		return zipcode;
+	}
+
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+
+	public String getCredit_card() {
+		return credit_card;
+	}
+
+	public void setCredit_card(String credit_card) {
+		this.credit_card = credit_card;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", email=" + email + ", password=" + password + ", address=" + address + ", state="
+				+ state + ", zipcode=" + zipcode + ", credit_card=" + credit_card + ", role=" + role + "]";
+	}
+	
 	
 	
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,1 @@
+insert into user (email, password, address, state, zipcode, credit_card, role) values ('user@user.com','password', '1234 apple st', 'NY', '12345', '123456789', 'admin');
