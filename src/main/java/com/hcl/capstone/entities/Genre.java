09beb35936a511059b878cd6756e436b7ad90091
Narges Goshayeshisafa <narges.gosha@hcl.com>
2021-02-28 16:53:10 -0800

    Added Genre and Song Entities

--- src/main/java/com/hcl/capstone/entities/Genre.java
+++ src/main/java/com/hcl/capstone/entities/Genre.java
@@ -1,5 +1,41 @@
 package com.hcl.capstone.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Genre {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // might have to change
+	private long id;
+	
+	private String name;
+	private String desctiption;
+	
+	public Genre() {
+		
+	}
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDesctiption() {
+		return desctiption;
+	}
+	public void setDesctiption(String desctiption) {
+		this.desctiption = desctiption;
+	}
+	
 }
