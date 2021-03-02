eda7813e213e5e68186b9e2713cc4ccfdafd438d
Connorrichter4 <Connorrichter4@gmail.com>
2021-03-02 10:12:04 -0600

    updated artist entity

--- src/main/java/com/hcl/capstone/entities/Artist.java
+++ src/main/java/com/hcl/capstone/entities/Artist.java
@@ -2,6 +2,7 @@ package com.hcl.capstone.entities;
 
 import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -9,13 +10,14 @@ import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 
+
 @Entity
 public class Artist {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY) // might have to change
 	private long id;
-	
+	@Column(nullable=false)
 	private String name;
 	private String location;
 	@OneToMany(mappedBy="artist") // changed from ManyToMany
